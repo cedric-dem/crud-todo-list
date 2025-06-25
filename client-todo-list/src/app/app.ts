890609e97e1cc0908996931a5941af1b20a9d4e0
cedric <demuelenaere.cedric@gmail.com>
2025-06-25 15:17:41 +0200

    create component to create a note in front end

--- client-todo-list/src/app/app.ts
+++ client-todo-list/src/app/app.ts
@@ -1,23 +1,57 @@
-import { RouterOutlet } from '@angular/router';
 import { Component, OnInit, inject } from '@angular/core';
+import { NoteService } from './services/note.service';
 import { TestService } from './services/test';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Note } from './models/note.model';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  styleUrl: './app.css',
-  template: `<p>{{ response }}</p>`,
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  styleUrls: ['./app.css'],
+  template: `
+    <h2>Crate a note</h2>
+    <form (ngSubmit)="submit()">
+      <input [(ngModel)]="note.title" name="title" placeholder="Title" required />
+      <br />
+      <textarea [(ngModel)]="note.content" name="content" placeholder="Content" required></textarea>
+      <br />
+      <button type="submit">Create</button>
+    </form>
+    <div *ngIf="createdNote">
+      <h3>Created Note</h3>
+      <p><strong>{{ createdNote.title }}</strong></p>
+      <p>{{ createdNote.content }}</p>
+    </div>
+
+    <p>{{ response }}</p>
+  `,
 })
 export class App implements OnInit {
-  protected title = 'client-todo-list';
+
+  private noteService = inject(NoteService);
   private testService = inject(TestService);
+
+  note: Note = { title: '', content: '' };
+  createdNote: Note | null = null;
+
   response = '';
 
   ngOnInit() {
     this.testService.getTest().subscribe({
       next: res => this.response = res,
       error: err => this.response = 'Error : ' + err.message
+    });
+  }
+
+  submit() {
+    this.noteService.createNote(this.note).subscribe({
+      next: res => {
+        this.createdNote = res;
+        this.note = { title: '', content: '' }; // reset
+      },
+      error: err => console.error('Error Creating note', err)
     });
   }
 }
